4c7cc0539ad03ac639e116691cfd9c6611670844
AlphaWang <1919wang@gmail.com>
2014-08-04 17:44:24 +0800

    format

--- src/ch01_declaration/Ch1_1_Initialize.java
+++ src/ch01_declaration/Ch1_1_Initialize.java
@@ -1,4 +1,4 @@
-/**               master
+/**               master  2
  * <p>文件名称: Ch1_1初始化.java </p>
  * <p>创建日期：2011-12-7</p>
  * @version 1.0
@@ -9,92 +9,91 @@ package ch01_declaration;
 import java.util.Arrays;
 
 /**
- * 
  * 非内部类的修饰符 不能为private！！：
  * only public, abstract & final are permitted
- *
+ * <p/>
  * Illegal modifier for the class privateClass; only public, abstract & final are permitted
  */
 //// private class privateClass{}
 //// private enum privateEnum{}
-enum DefaultEnum{
-	SMALL,BIG,LARGE;
-	DefaultEnum(){
+enum DefaultEnum {
+	SMALL,
+	BIG,
+	LARGE;
+
+	DefaultEnum() {
 		System.out.println("Enum构造函数，不可直接调用");
 	}
 }
 
 public class Ch1_1_Initialize {
-	
+
 	private String name;
 	public String publicVar = "父类publicVar";
 	public String publicVar2 = "父类publicVar2";
-	
-	
+
 	{
 		publicVar = "父类publicVar!";
 		System.out.println("父类成员初始化块");
 		//// blankFinalVar = 0;       //空白final，不可多次赋值
 	}
-	
-	
+
 	private final int blankFinalVar;
-	
-	Ch1_1_Initialize(){
+
+	Ch1_1_Initialize() {
 		blankFinalVar = 1;
 		System.out.println("父类构造方法()");
 	}
-	Ch1_1_Initialize(String name){
-		blankFinalVar = 2;          //空白final，必须在构造器之前对其赋值;
+
+	Ch1_1_Initialize(String name) {
+		blankFinalVar = 2; //空白final，必须在构造器之前对其赋值;
 		//// blankFinalVar = 3;      //空白final，不可多次赋值
 		System.out.println("父类构造方法(String)");
 		this.name = name;
 	}
-	
-	
-	void print(){
+
+	void print() {
 		System.out.println("父类print()：" + name);
 	}
+
 	/**
 	 * 被继承后，调用子类的print()
 	 */
-	void callPrint1(){
+	void callPrint1() {
 		print();
 	}
-	
-	
-	
+
 	/**
 	 * final方法参数
 	 */
-	void setName(final String n){
+	void setName(final String n) {
 		this.name = n;
 		name = "b";
-		
+
 		// final参数，不可更改引用
 		///// n = "";
 	}
-	String getName(){
+
+	String getName() {
 		return this.name;
 	}
+
 	/**
 	 * private方法，不可被继承
 	 */
-	private void privateMethod(){
-		System.out.println("父类privateMethod()：" );
+	private void privateMethod() {
+		System.out.println("父类privateMethod()：");
 		//可用values()遍历枚举元素
 		DefaultEnum[] eItems = DefaultEnum.values();
 		System.out.println(Arrays.asList(eItems));
-		
+
 		//Enum构造函数，不可直接调用
 		//// DefaultEnum e1 = new DefaultEnum();
 		DefaultEnum e2 = DefaultEnum.BIG;
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		new Ch1_1_Initialize().privateMethod();
 	}
-	
-	
 
 }
--- src/ch01_declaration/Ch1_1_InitializeSub.java
+++ src/ch01_declaration/Ch1_1_InitializeSub.java
@@ -6,26 +6,25 @@
  */
 package ch01_declaration;
 
-public class Ch1_1_InitializeSub extends Ch1_1_Initialize{
+public class Ch1_1_InitializeSub extends Ch1_1_Initialize {
 	/**
 	 * ？？————变量不能被继承?? 这里是新建变量publicVar
 	 * 
 	 * 域能继承、域不能多态。
 	 */
 	public String publicVar = "子类publicVar";
-	
-	
+
 	{
 		publicVar = "子类publicVar!";
 		System.out.println("子类成员初始化块");
 	}
-	
-	
-	Ch1_1_InitializeSub(){
+
+	Ch1_1_InitializeSub() {
 		super("alpha");
 		System.out.println("子类构造方法()");
 	}
-	Ch1_1_InitializeSub(String name){
+
+	Ch1_1_InitializeSub(String name) {
 		// 此行自动添加super()
 		System.out.println("子类构造方法(String name)");
 		/*
@@ -36,29 +35,29 @@ public class Ch1_1_InitializeSub extends Ch1_1_Initialize{
 		//// super.name = name;
 		setName(name);
 	}
-	
+
 	@Override
-	void print(){
+	void print() {
 		System.out.println("子类print() " + getName());
 	}
-	
+
 	//不能覆盖private方法
 	//// @Override
-	void privateMethod(){
-		System.out.println("子类privateMethod()：" );
+	void privateMethod() {
+		System.out.println("子类privateMethod()：");
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		Ch1_1_InitializeSub sub1 = new Ch1_1_InitializeSub();
 		sub1.print();
 		sub1.callPrint1();
-		
+
 		System.out.println("----------");
 		Ch1_1_Initialize sub2 = new Ch1_1_InitializeSub("beta");//多态
 		sub2.print();
 		System.out.println("----------");
 		sub2.callPrint1(); //被继承后，调用子类的print()
-		
+
 		/**
 		 * 测试域的继承、多态
 		 * 
@@ -69,11 +68,11 @@ public class Ch1_1_InitializeSub extends Ch1_1_Initialize{
 		System.out.println(sub1.publicVar2);//父类publicVar2   --如果不能被继承，则应报错？                           --域能继承
 		System.out.println(sub2.publicVar); //父类publicVar！！ --如果能被继承，则应输出子类publicVar --域不能多态
 		System.out.println(sub2.publicVar2);//父类publicVar2
-		
+
 		sub1.privateMethod();
 		//The method privateMethod() from the type Ch1_1_初始化 is not visible
 		//// sub2.privateMethod();
-		
+
 	}
 
 }
